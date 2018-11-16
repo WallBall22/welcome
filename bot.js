d070b27592de2a6f07f641118179b56998ae9fbc
WallBall22 <44549704+WallBall22@users.noreply.github.com>
2018-11-16 10:33:56 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
